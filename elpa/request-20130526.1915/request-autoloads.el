259cbbe4e1c0969110df58e196d252ae43339929
svs <svs@svs.io>
2014-08-23 16:48:35 +0530

    updates

---
+++ elpa/request-20130526.1915/request-autoloads.el
@@ -0,0 +1,18 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("request-deferred.el" "request-pkg.el"
+;;;;;;  "request.el") (21137 51140 900169))
+
+;;;***
+
+(provide 'request-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; request-autoloads.el ends here
---
+++ elpa/s-20131223.944/s-autoloads.el
@@ -0,0 +1,17 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21215 44503 22931))
+
+;;;***
+
+(provide 's-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-autoloads.el ends here

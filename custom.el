18e233f0369b8d7eb76a22196469b2caf77c7677
svs-iv <svs@invideo.io>
2022-12-16 13:41:09 +0530

    huge updates

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(zenburn-theme web-mode use-package tide speedbar-git-respect solarized-theme slim-mode seeing-is-believing ruby-test-mode ruby-electric rspec-mode rbenv projectile-rails multiple-cursors magit logito json-mode js2-mode helm-projectile helm-ag enh-ruby-mode eglot-fsharp edn company cider better-defaults)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

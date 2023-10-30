1bb8d1ae6b9b23fb54ad55c207455654a1ee2a8f
Siddharth <svs@svs.io>
2023-10-30 14:20:15 +0530

    loads a changes

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(enh-ruby-mode zenburn-theme yaml-mode solarized-theme slim-mode seeing-is-believing ruby-test-mode ruby-electric rspec-mode rbenv projectile-rails multiple-cursors magit logito helm-projectile helm-ag exec-path-from-shell eglot-fsharp editorconfig better-defaults)))
+ '(ruby-test-rspec-options '("")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
